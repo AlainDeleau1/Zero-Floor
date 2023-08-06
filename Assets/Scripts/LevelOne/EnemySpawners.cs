a57bd2fce6ee56690615e55e5890386b7a9ae8df
ffeIipe <felipealderete03@gmail.com>
2023-08-05 23:16:40 -0300

    spawners fixeados

--- Assets/Scripts/LevelOne/EnemySpawners.cs
+++ Assets/Scripts/LevelOne/EnemySpawners.cs
@@ -9,12 +9,7 @@ public class EnemySpawners : MonoBehaviour
     public Transform player;
     public int rangeEnemySpawner;
     public int instantiated;
-    public int enemiesPerWave;
-
-    private void Start()
-    {
-        enemiesPerWave = 14;
-    }
+    public int enemiesPerWave = 7;
 
     private void Update()
     {
@@ -25,44 +20,23 @@ public class EnemySpawners : MonoBehaviour
         }
     }
 
-    private void SpawnEnemies()
+    public void SpawnEnemies()
     {
         for (int i = 0; i < enemySpawners.Length; i++)
         {
             float distance = Vector3.Distance(enemySpawners[i].position, player.position);
-
-            int chance = Random.Range(0, 100);
 
             if (distance > rangeEnemySpawner && instantiated <= enemiesPerWave)
             {
                 int randomIndex = Random.Range(0, enemySpawners.Length);
                 Vector3 randomPosition = enemySpawners[randomIndex].position;
 
-                if (chance > 50)
-                {
-                    Instantiate(enemyPrefab, randomPosition, enemySpawners[i].rotation);
-                    instantiated++;
-                }
-                else if (chance < 50 && chance > 25)
-                {
-                    Instantiate(shooterEnemy, randomPosition, enemySpawners[i].rotation);
-                    instantiated++;
-                }
-                else if (chance < 25 && chance > 15)
-                {
-                    Instantiate(ninjaEnemy, randomPosition, enemySpawners[i].rotation);
-                    instantiated++;
-                }
-                else
-                {
-                    Instantiate(bigEnemy, randomPosition, enemySpawners[i].rotation);
-                    instantiated++;
-                }
+                Instantiate(enemyPrefab, randomPosition, enemySpawners[i].rotation);
+                instantiated++;
             }
         }
         instantiated = 0;
     }
-
 
     private async void OnEnable()
     {
