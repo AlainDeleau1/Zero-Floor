dce8c3d5e0bfbf1c0aa3840f1ca39c13ecde9e8a
AlainDeleau <136400785+AlainDeleau1@users.noreply.github.com>
2023-08-11 15:16:06 -0300

    Cambios Level 6

--- Assets/Scripts/Levels/Spawners/EnemySpawners6.cs
+++ Assets/Scripts/Levels/Spawners/EnemySpawners6.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 public class EnemySpawners6 : EnemySpawners
 {
-    private void Awake()
-    {
-        maxKills = 110;
-        print(maxKills);
-        enemiesPerWave += 3;
-        print(enemiesPerWave);
-    }
     private void Update()
     {
         if (spawned == false)
@@ -23,14 +16,33 @@ public class EnemySpawners6 : EnemySpawners
         for (int i = 0; i < enemySpawners.Length; i++)
         {
             float distance = Vector3.Distance(enemySpawners[i].position, player.position);
+            int chance = Random.Range(0, 100);
 
             if (distance > rangeEnemySpawner && instantiated <= enemiesPerWave)
             {
                 int randomIndex = Random.Range(0, enemySpawners.Length);
                 Vector3 randomPosition = enemySpawners[randomIndex].position;
 
-                Instantiate(enemyPrefab, randomPosition, enemySpawners[i].rotation);
-                instantiated++;
+                if (chance > 50)
+                {
+                    Instantiate(enemyPrefab, randomPosition, enemySpawners[i].rotation);
+                    instantiated++;
+                }
+                else if (chance < 50 && chance > 25)
+                {
+                    Instantiate(shooterEnemy, randomPosition, enemySpawners[i].rotation);
+                    instantiated++;
+                }
+                else if (chance < 25 && chance > 15)
+                {
+                    Instantiate(ninjaEnemy, randomPosition, enemySpawners[i].rotation);
+                    instantiated++;
+                }
+                else
+                {
+                    Instantiate(bigEnemy, randomPosition, enemySpawners[i].rotation);
+                    instantiated++;
+                }
             }
         }
         instantiated = 0;
